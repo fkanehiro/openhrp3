f690a68af838fdb6bab988b6c5ed8da53fcd8737
fsi-takahashi <fsi-takahashi@5146e84e-5855-4829-b829-26b81c797afe>
2008-07-01 02:19:03 +0000

    Copyright表示などを反映

--- OpenHRP/Base/TimeMeasure.h
+++ OpenHRP/Base/TimeMeasure.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2008, AIST, the University of Tokyo and General Robotix Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * National Institute of Advanced Industrial Science and Technology (AIST)
+ * General Robotix Inc. 
+ */
 
 #ifndef OPENHRP_TIME_MEASURE_H_INCLUDED
 #define OPENHRP_TIME_MEASURE_H_INCLUDED
@@ -41,16 +50,44 @@ class TimeMeasure
 
 
 #else
+#include <windows.h>
+typedef unsigned __int64    ulonglong;
 
 class TimeMeasure
 {
- public:
-    TimeMeasure() { }
-    void begin() { }
-    void end(){ }
-	double time() { return 0.0; }
-    double totalTime() { return 0.0; }
-    double avarageTime() { return 0.0; }
+	ulonglong iTimerScale;
+	ulonglong beginTime;
+	ulonglong endTime;
+	double time_;
+	double totalTime_;
+    int numCalls;
+ 
+	public:
+    TimeMeasure() { 
+		totalTime_ = 0.0;
+		numCalls = 0;
+		BOOL iDummyBool = QueryPerformanceFrequency ((LARGE_INTEGER *) &iTimerScale);
+		if(!iDummyBool)
+			iTimerScale=1;
+	}
+
+    void begin() { 
+		BOOL iDummyBool = QueryPerformanceCounter ((LARGE_INTEGER *) &beginTime);
+		if(!iDummyBool)
+			beginTime=1;
+	}
+
+    void end(){ 
+		BOOL iDummyBool = QueryPerformanceCounter ((LARGE_INTEGER *) &endTime);
+		if(!iDummyBool)
+			endTime=0;
+		time_ = (double)(endTime - beginTime) / iTimerScale;
+		totalTime_ += time_;
+		numCalls++;
+	}
+	double time() { return time_; }
+    double totalTime() { return totalTime_; }
+    double avarageTime() { return totalTime_ / numCalls; }
 };
 
 #endif
