aea0070221633362f0cec9e8f66c8a22a1e69ce4
nakaoka <nakaoka@5146e84e-5855-4829-b829-26b81c797afe>
2008-07-06 15:26:51 +0000

    コンパイルに cmake を使用するようにし、MakefileをCMakeLists.txt で置き換えた。

--- bin/unix/GrxUI.sh
+++ bin/unix/GrxUI.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 . ./config.sh
-export CLASSPATH=$CLASSPATH:grxui.jar:$JYTHON_DIR/jython.jar:$HRP_STUB_PATH
 
 ##
 # GrxUI usage:
@@ -16,26 +15,24 @@ DEBUG=$4
 ## 
 # for client/gui/grxuirc.xml
 ##
-BIN_DIR=$OPENHRPHOME/bin/unix
+BIN_DIR=${OPENHRPHOME}/bin/unix
 BIN_SFX=.sh
 
-if [ -f $OPENHRPHOME/bin/unix/omninames-*.log ] ; then
-/bin/rm $OPENHRPHOME/bin/unix/omninames-*.*;
+if [ -f ${OPENHRPHOME}/bin/unix/omninames-*.log ] ; then
+/bin/rm ${OPENHRPHOME}/bin/unix/omninames-*.*;
 fi
 
 cd ../../client/gui
-$JAVAVM -server -Xmx512m \
-	$EXTRA_JAVA_FLAGS  \
-	-DPROJECT="$DEFAULT_PROJECT" \
-	-DMODE="$DEFAULT_MODE" \
-	-DSCRIPT="$SCRIPT" \
-	-DDEBUG=$DEBUG \
-	-DNS_OPT="$NS_OPT" \
-	-DOPENHRPHOME=$OPENHRPHOME \
-	-DBIN_DIR=$BIN_DIR \
-	-DBIN_SFX=$BIN_SFX \
-	-Dpython.home=$JYTHON_DIR \
-	-Dpython.path=$JYTHON_DIR/Lib:$JYTHON_DIR/Lib-cpython \
+${JAVAVM} -server -Xmx512m \
+	-DPROJECT="${DEFAULT_PROJECT}" \
+	-DMODE="${DEFAULT_MODE}" \
+	-DSCRIPT="{$SCRIPT}" \
+	-DDEBUG=${DEBUG} \
+	-DNS_OPT="${NS_OPT}" \
+	-DOPENHRPHOME=${OPENHRPHOME} \
+	-DBIN_DIR=${BIN_DIR} \
+	-DBIN_SFX=${BIN_SFX} \
+	-Dpython.home=${JYTHON_HOME} \
 	-Dpython.cachedir=$HOME/.jython-cache \
 	com.generalrobotix.ui.GrxUI
 
