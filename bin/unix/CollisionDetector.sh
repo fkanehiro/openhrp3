e8bc139cbf7beb5c0e99986b0860910494a7e95f
nakaoka <nakaoka@5146e84e-5855-4829-b829-26b81c797afe>
2008-07-10 13:56:59 +0000

    bin 以下のスクリプトを整理

--- bin/unix/CollisionDetector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. config.sh
-cd $OPENHRPHOME/bin
-./openhrp-collision-detector $NS_OPT
-
-
--- bin/unix/ModelLoader.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. config.sh
-cd $OPENHRPHOME/bin
-./openhrp-model-loader $NS_OPT
