f1541f014355c110676bd01c04e45a7271ff791a
fsi-takahashi <fsi-takahashi@5146e84e-5855-4829-b829-26b81c797afe>
2008-09-05 00:23:45 +0000

    JoystickControlサンプル追加

---
+++ sample/JoystickControl/Controller/JoystickController.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+openhrp-controller-bridge \
+--server-name JoystickController \
+--out-port angle:JOINT_VALUE \
+--out-port velocity:JOINT_VELOCITY \
+--in-port torque:JOINT_TORQUE \
+--connection angle:JoystickController0:angle \
+--connection velocity:JoystickController0:velocity \
+--connection torque:JoystickController0:torque
