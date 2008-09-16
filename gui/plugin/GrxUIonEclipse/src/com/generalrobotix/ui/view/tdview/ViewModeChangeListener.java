c2731f1cb21058a3e30b2abbcd6de96895ff2a48
fsi-takahashi <fsi-takahashi@5146e84e-5855-4829-b829-26b81c797afe>
2008-09-16 04:03:51 +0000

    古くなっているので削除

--- gui/plugin/GrxUIonEclipse/src/com/generalrobotix/ui/view/tdview/ViewModeChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.generalrobotix.ui.view.tdview;
-
-import java.util.EventListener;
-
-public interface ViewModeChangeListener extends EventListener {
-    public void viewModeChanged(int mode);
-}
