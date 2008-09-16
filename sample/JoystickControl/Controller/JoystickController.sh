6ba42ce75abbbe1a5c2479cee5ed053afe9012e1
fsi-takahashi <fsi-takahashi@5146e84e-5855-4829-b829-26b81c797afe>
2008-09-16 06:06:56 +0000

    ホームページの記述に合わせて、自動接続しないように変更

--- sample/JoystickControl/Controller/JoystickController.sh
+++ sample/JoystickControl/Controller/JoystickController.sh
@@ -5,6 +5,3 @@ openhrp-controller-bridge \
 --out-port angle:JOINT_VALUE \
 --out-port velocity:JOINT_VELOCITY \
 --in-port torque:JOINT_TORQUE \
---connection angle:JoystickController0:angle \
---connection velocity:JoystickController0:velocity \
---connection torque:JoystickController0:torque
