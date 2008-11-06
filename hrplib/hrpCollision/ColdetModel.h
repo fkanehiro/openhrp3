b0aa8213938ced457c26f3981ba0cacfc43bfd5e
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2008-11-06 06:42:40 +0000

    * primitive information is added to ColdetModel * special collision detector between a plane and a cylinder is added

--- hrplib/hrpCollision/ColdetModel.h
+++ hrplib/hrpCollision/ColdetModel.h
@@ -30,31 +30,144 @@ namespace hrp {
     class HRP_COLLISION_EXPORT ColdetModel
     {
       public:
+        enum PrimitiveType { SP_MESH, SP_BOX, SP_CYLINDER, SP_CONE, SP_SPHERE, SP_PLANE };
+
+        /**
+         * @brief constructor
+         */
         ColdetModel();
+
+        /**
+         * @brief copy constructor
+         *
+         * Shape information stored in dataSet is shared with org
+         */
         ColdetModel(const ColdetModel& org);
+
+        /**
+         * @brief destructor
+         */
         virtual ~ColdetModel();
 
+        /**
+         * @brief set name of this model
+         * @param name name of this model 
+         */
         void setName(const char* name) { name_ = name; }
-        const char* name() { return name_.c_str(); }
 
+        /**
+         * @brief get name of this model
+         * @return name name of this model 
+         */
+        const char* name() const { return name_.c_str(); }
+
+        /**
+         * @brief set the number of vertices
+         * @param n the number of vertices
+         */
         void setNumVertices(int n);
+
+        /**
+         * @brief set the number of triangles
+         * @param n the number of triangles
+         */
         void setNumTriangles(int n);
-        
+
+        /**
+         * @brief add a vertex
+         * @param index index of the vertex
+         * @param x x position of the vertex
+         * @param y y position of the vertex
+         * @param z z position of the vertex
+         */
         void setVertex(int index, float x, float y, float z);
+
+        /**
+         * @brief add a triangle
+         * @param index index of the triangle
+         * @param v1 index of the first vertex
+         * @param v2 index of the second vertex
+         * @param v3 index of the third vertex
+         */
         void setTriangle(int index, int v1, int v2, int v3);
 
+        /**
+         * @brief build tree of bounding boxes to accelerate collision check
+         *
+         * This method must be called before doing collision check
+         */
         void build();
 
-        bool isValid() { return isValid_; }
+        /**
+         * @brief check if build() is already called or not
+         * @return true if build() is already called, false otherwise
+         */
+        bool isValid() const { return isValid_; }
 
+        /**
+         * @brief set position and orientation of this model
+         * @param R new orientation 
+         * @param p new position
+         */
         void setPosition(const Matrix33& R, const Vector3& p);
+
+        /**
+         * @brief set position and orientation of this model
+         * @param R new orientation (length = 9)  
+         * @param p new position (length = 3)
+         */
         void setPosition(const double* R, const double* p);
 
+        /**
+         * @brief set primitive type
+         * @param ptype primitive type
+         */
+        void setPrimitiveType(PrimitiveType ptype);
+
+        /**
+         * @brief get primitive type
+         * @return primitive type
+         */
+        PrimitiveType getPrimitiveType() const;
+
+        /**
+         * @brief set the number of parameters of primitive
+         * @nparam the number of parameters of primitive
+         */
+        void setNumPrimitiveParams(unsigned int nparam);
+
+        /**
+         * @brief set a parameter of primitive
+         * @param index index of the parameter
+         * @param value value of the parameter
+         * @return true if the parameter is set successfully, false otherwise
+         */
+        bool setPrimitiveParam(unsigned int index, float value);
+
+        /**
+         * @brief get a parameter of primitive
+         * @param index index of the parameter
+         * @param value value of the parameter
+         * @return true if the parameter is gotten successfully, false otherwise
+         */
+        bool getPrimitiveParam(unsigned int index, float &value) const;
+
+        /**
+         * @brief set position and orientation of primitive
+         * @param R orientation relative to link (length = 9)  
+         * @param p position relative to link (length = 3)
+         */
+        void setPrimitivePosition(const double* R, const double* p);
+
       private:
+        /**
+         * @brief common part of constuctors
+         */
         void initialize();
         
         ColdetModelSharedDataSet* dataSet;
         IceMaths::Matrix4x4* transform;
+        IceMaths::Matrix4x4* pTransform; ///< transform of primitive
         std::string name_;
         bool isValid_;
 
--- hrplib/hrpCollision/ColdetModelPair.cpp
+++ hrplib/hrpCollision/ColdetModelPair.cpp
@@ -52,14 +52,26 @@ void ColdetModelPair::set(ColdetModelPtr model0, ColdetModelPtr model1)
 
 collision_data* ColdetModelPair::detectCollisionsSub(bool detectAllContacts)
 {
-    collision_data* result = 0;
-    
-    if(cdContact){
-        delete[] cdContact;
-        cdContact = 0;
+    if(cdContactsCount){
+        if (cdContact){
+            delete[] cdContact;
+            cdContact = 0;
+        }
         cdContactsCount = 0;
     }
 
+    if (model0_->getPrimitiveType() == ColdetModel::SP_PLANE &&
+        model1_->getPrimitiveType() == ColdetModel::SP_CYLINDER){
+        return detectPlaneCylinderCollisions(detectAllContacts);
+    }else{
+        return detectMeshMeshCollisions(detectAllContacts);
+    }
+}
+
+collision_data* ColdetModelPair::detectMeshMeshCollisions(bool detectAllContacts)
+{
+    collision_data* result = 0;
+    
     if(model0_->isValid() && model1_->isValid()){
 
         Opcode::BVTCache colCache;
@@ -86,4 +98,76 @@ collision_data* ColdetModelPair::detectCollisionsSub(bool detectAllContacts)
     }
 
     return result;
+}
+
+collision_data* ColdetModelPair::detectPlaneCylinderCollisions(bool detectAllContacts)
+{
+    IceMaths::Matrix4x4 pTrans = (*(model0_->pTransform)) * (*(model0_->transform));
+    IceMaths::Matrix4x4 cTrans = (*(model1_->pTransform)) * (*(model1_->transform));
+
+    float radius, height; // height and radius of cylinder
+    model1_->getPrimitiveParam(0, radius);
+    model1_->getPrimitiveParam(1, height);
+
+    IceMaths::Point pTopLocal(0, height/2, 0), pBottomLocal(0, -height/2, 0);
+    IceMaths::Point pTop, pBottom; // center points of top and bottom discs
+    IceMaths::TransformPoint4x3(pTop,    pTopLocal,    cTrans);
+    IceMaths::TransformPoint4x3(pBottom, pBottomLocal, cTrans);
+    
+    IceMaths::Point pOnPlane, nLocal(0,0,1), n;
+    IceMaths::TransformPoint3x3(n, nLocal, pTrans);
+    pTrans.GetTrans(pOnPlane);
+    float d = pOnPlane|n; // distance between origin and plane
+
+    float dTop    = (pTop|n) - d;
+    float dBottom = (pBottom|n) - d;
+
+    if (dTop > radius && dBottom > radius) return NULL;
+
+    double theta = asin((dTop - dBottom)/height);
+    double rcosth = radius*cos(theta);
+
+    if (rcosth >= dTop) cdContactsCount++;
+    if (rcosth >= dBottom) cdContactsCount++;
+
+    if (cdContactsCount){
+        collision_data *cdata = new collision_data[cdContactsCount];
+        for (unsigned int i=0; i<cdContactsCount; i++){
+            cdata[i].num_of_i_points = 1;
+            cdata[i].i_point_new[0]=1; 
+            cdata[i].i_point_new[1]=0; 
+            cdata[i].i_point_new[2]=0; 
+            cdata[i].i_point_new[3]=0; 
+            cdata[i].n_vector[0] = n.x;
+            cdata[i].n_vector[1] = n.y;
+            cdata[i].n_vector[2] = n.z;
+        }
+        IceMaths::Point vBottomTop = pTop - pBottom;
+        IceMaths::Point v = vBottomTop^n;
+        IceMaths::Point w = v^n;
+        w.Normalize();
+
+        unsigned int index=0;
+        if (rcosth >= dBottom){ // bottom disc collides
+            double depth = rcosth - dBottom;
+            IceMaths::Point iPoint = pBottom - dBottom*n - dBottom*tan(theta)*w;
+            cdata[index].i_points[0][0] = iPoint.x;
+            cdata[index].i_points[0][1] = iPoint.y;
+            cdata[index].i_points[0][2] = iPoint.z;
+            cdata[index].depth = depth;
+            index++;
+        }
+        if (rcosth >= dTop){ // top disc collides
+            double depth = rcosth - dTop;
+            IceMaths::Point iPoint = pTop - dTop*n - dTop*tan(theta)*w;
+            cdata[index].i_points[0][0] = iPoint.x;
+            cdata[index].i_points[0][1] = iPoint.y;
+            cdata[index].i_points[0][2] = iPoint.z;
+            cdata[index].depth = depth;
+            index++;
+        }
+
+        return cdata;
+    }
+    return NULL;
 }
