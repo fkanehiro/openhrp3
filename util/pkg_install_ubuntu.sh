2d6930474db53445b6249476c055bc3d580d69e5
warataka <warataka@5146e84e-5855-4829-b829-26b81c797afe>
2008-11-17 08:51:26 +0000

    チケット#239 の作業を更新

---
+++ util/pkg_install_ubuntu.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+#
+# @file pkg_install_ubuntu.sh
+# @brief OpenRTM-aist dependent packages install script for Debian-sarge
+# @author Noriaki Ando <n-ando@aist.go.jp>
+#         Shinji Kurihara
+#         Tetsuo Ando
+#         Harumi Miyamoto
+#
+
+#---------------------------------------
+# パッケージリスト
+#---------------------------------------
+omni="libomniorb4 libomniorb4-dev omniidl4 omniorb4-nameserver"
+ace="libace libace-dev"
+openrtm="openrtm-aist openrtm-aist-doc openrtm-aist-dev openrtm-aist-example python-yaml"
+
+devel="gcc g++ make"
+packages="$devel $omni $ace $openrtm"
+u_packages="$omni $ace $openrtm "
+
+#---------------------------------------
+# リポジトリサーバ
+#---------------------------------------
+create_srclist () {
+    cnames="sarge edgy feisty gutsy hardy"
+    for c in $cnames; do
+	if test -f "/etc/apt/sources.list"; then
+	    res=`grep $c /etc/apt/sources.list`
+	else
+	    echo "This distribution may not be debian/ubuntu."
+	    exit
+	fi
+	if test ! "x$res" = "x" ; then
+	    code_name=$c
+	fi
+    done
+    if test ! "x$code_name" = "x"; then
+	echo "The code name is : " $code_name
+    else
+	echo "This OS is not supported."
+	exit
+    fi
+    openrtm_repo="deb http://www.openrtm.org/pub/Linux/ubuntu/ $code_name main"
+}
+
+#---------------------------------------
+# ソースリスト更新関数の定義
+#---------------------------------------
+update_source_list () {
+    rtmsite=`grep openrtm /etc/apt/sources.list`
+    if test "x$rtmsite" = "x" ; then
+	echo "OpenRTM-aist のリポジトリが登録されていません。"
+	echo "Source.list に OpenRTM-aist のリポジトリ: "
+	echo "  " $openrtm_repo
+	read -p "を追加します。よろしいですか？ (y/n) [y] " kick_shell
+
+	if test "x$kick_shell" = "xn" ; then
+	    echo "中断します。"
+	    exit 0
+	else
+	    echo $openrtm_repo >> /etc/apt/sources.list
+	fi
+    fi
+}
+
+#----------------------------------------
+# root かどうかをチェック
+#----------------------------------------
+check_root () {
+    if test ! `id -u` = 0 ; then
+	echo ""
+	echo "This script should be run by root user."
+	echo "Abort."
+	echo ""
+	exit 1
+    fi
+}
+
+#----------------------------------------
+# パッケージインストール関数
+#----------------------------------------
+install_packages () {
+    for p in $*; do
+	echo "Now installing: " $p
+	apt-get install $p
+	echo "done."
+	echo ""
+    done
+}
+
+#------------------------------------------------------------
+# リストを逆順にする
+#------------------------------------------------------------
+reverse () {
+    for i in $*; do
+	echo $i
+    done | sed '1!G;h;$!d'
+}
+
+#----------------------------------------
+# パッケージをアンインストールする
+#----------------------------------------
+uninstall_packages () {
+    for p in $*; do
+	echo "Now uninstalling: " $p
+	aptitude remove $p
+	echo "done."
+	echo ""
+    done
+}
+
+#---------------------------------------
+# メイン
+#---------------------------------------
+check_root
+if test "x$1" = "x-u" ; then
+    uninstall_packages `reverse $u_packages`
+else
+    create_srclist
+    update_source_list
+    apt-get update
+    install_packages $packages
+fi
+
