0fc0565e4eaf11568b641388db4db7ac10c959a3
nakaoka <nakaoka@5146e84e-5855-4829-b829-26b81c797afe>
2009-06-22 16:11:43 +0000

    added functions for accessing arrays of links to the Body class

--- hrplib/hrpBase/LinkTraverse.cpp
+++ hrplib/hrpBase/LinkTraverse.cpp
@@ -1,4 +1,3 @@
-// -*- mode: c++; indent-tabs-mode: t; tab-width: 4; c-basic-offset: 4; -*-
 /*
  * Copyright (c) 2008, AIST, the University of Tokyo and General Robotix Inc.
  * All rights reserved. This program is made available under the terms of the
@@ -10,16 +9,16 @@
  */
 
 /** 
-	\file
-	\brief Implementations of the LinkTraverse class
-	\author Shin'ichiro Nakaoka
+    \file
+    \brief Implementations of the LinkTraverse class
+    \author Shin'ichiro Nakaoka
 */
   
 
 #include "LinkTraverse.h"
 #include "Link.h"
-#include <hrpUtil/Tvmet3d.h>
 #include <algorithm>
+#include <hrpUtil/Tvmet3d.h>
 
 using namespace std;
 using namespace hrp;
@@ -32,8 +31,8 @@ LinkTraverse::LinkTraverse()
 }
 
 
-LinkTraverse::LinkTraverse(int size) : 
-	links(size)
+LinkTraverse::LinkTraverse(int size)
+    : links(size)
 {
     links.clear();
 }
@@ -91,55 +90,55 @@ void LinkTraverse::calcForwardKinematics(bool calcVelocity, bool calcAcceleratio
         switch(child->jointType){
 
         case Link::ROTATIONAL_JOINT:
-			link->R = child->R * trans(rodrigues(child->a, child->q));
-			arm = link->R * child->b;
-			link->p = child->p - arm;
+            link->R = child->R * trans(rodrigues(child->a, child->q));
+            arm = link->R * child->b;
+            link->p = child->p - arm;
 
-			if(calcVelocity){
-				child->sw = link->R * child->a;
-				link->w = child->w - child->dq * child->sw;
-				link->v = child->v - cross(link->w, arm);
+            if(calcVelocity){
+                child->sw = link->R * child->a;
+                link->w = child->w - child->dq * child->sw;
+                link->v = child->v - cross(link->w, arm);
 
-				if(calcAcceleration){
+                if(calcAcceleration){
                     link->dw = child->dw - child->dq * cross(child->w, child->sw) - (child->ddq * child->sw);
-					link->dv = child->dv - cross(child->w, cross(child->w, arm)) - cross(child->dw, arm);
-				}
-			}
+                    link->dv = child->dv - cross(child->w, cross(child->w, arm)) - cross(child->dw, arm);
+                }
+            }
             break;
             
         case Link::SLIDE_JOINT:
             link->R = child->R;
-			arm = link->R * (child->b + child->q * child->d);
+            arm = link->R * (child->b + child->q * child->d);
             link->p = child->p - arm;
 
-			if(calcVelocity){
-				child->sv = link->R * child->d;
-				link->w = child->w;
-				link->v = child->v - child->dq * child->sv;
+            if(calcVelocity){
+                child->sv = link->R * child->d;
+                link->w = child->w;
+                link->v = child->v - child->dq * child->sv;
 
-				if(calcAcceleration){
-					link->dw = child->dw;
-					link->dv = child->dv - cross(child->w, cross(child->w, arm)) - cross(child->dw, arm)
-						- 2.0 * child->dq * cross(child->w, child->sv) - child->ddq * child->sv;
-				}
-			}
+                if(calcAcceleration){
+                    link->dw = child->dw;
+                    link->dv = child->dv - cross(child->w, cross(child->w, arm)) - cross(child->dw, arm)
+                        - 2.0 * child->dq * cross(child->w, child->sv) - child->ddq * child->sv;
+                }
+            }
             break;
             
         case Link::FIXED_JOINT:
         default:
             link->R = child->R;
-			link->p = child->p - (link->R * child->b);
+            link->p = child->p - (link->R * child->b);
 
-			if(calcVelocity){
-				link->w = child->w;
-				link->v = child->v;
+            if(calcVelocity){
+                link->w = child->w;
+                link->v = child->v;
 				
-				if(calcAcceleration){
-					link->dw = child->dw;
-					link->dv = child->dv;
-				}
-			}
-			break;
+                if(calcAcceleration){
+                    link->dw = child->dw;
+                    link->dv = child->dv;
+                }
+            }
+            break;
         }
     }
 
@@ -152,55 +151,55 @@ void LinkTraverse::calcForwardKinematics(bool calcVelocity, bool calcAcceleratio
         switch(link->jointType){
             
         case Link::ROTATIONAL_JOINT:
-			link->R = parent->R * rodrigues(link->a, link->q);
-			arm = parent->R * link->b;
-			link->p = parent->p + arm;
+            link->R = parent->R * rodrigues(link->a, link->q);
+            arm = parent->R * link->b;
+            link->p = parent->p + arm;
 
-			if(calcVelocity){
-				link->sw = parent->R * link->a;
-				link->w = parent->w + link->sw * link->dq;
-				link->v = parent->v + cross(parent->w, arm);
+            if(calcVelocity){
+                link->sw = parent->R * link->a;
+                link->w = parent->w + link->sw * link->dq;
+                link->v = parent->v + cross(parent->w, arm);
 
-				if(calcAcceleration){
-					link->dw = parent->dw + link->dq * cross(parent->w, link->sw) + (link->ddq * link->sw);
-					link->dv = parent->dv + cross(parent->w, cross(parent->w, arm)) + cross(parent->dw, arm);
-				}
-			}
+                if(calcAcceleration){
+                    link->dw = parent->dw + link->dq * cross(parent->w, link->sw) + (link->ddq * link->sw);
+                    link->dv = parent->dv + cross(parent->w, cross(parent->w, arm)) + cross(parent->dw, arm);
+                }
+            }
             break;
             
         case Link::SLIDE_JOINT:
             link->R = parent->R;
-			arm = parent->R * (link->b + link->q * link->d);
-			link->p = parent->p + arm;
+            arm = parent->R * (link->b + link->q * link->d);
+            link->p = parent->p + arm;
 
-			if(calcVelocity){
-				link->sv = parent->R * link->d;
-				link->w = parent->w;
-				link->v = parent->v + link->sv * link->dq;
+            if(calcVelocity){
+                link->sv = parent->R * link->d;
+                link->w = parent->w;
+                link->v = parent->v + link->sv * link->dq;
 
-				if(calcAcceleration){
-					link->dw = parent->dw;
-					link->dv = parent->dv + cross(parent->w, cross(parent->w, arm)) + cross(parent->dw, arm)
-						+ 2.0 * link->dq * cross(parent->w, link->sv) + link->ddq * link->sv;
-				}
-			}
+                if(calcAcceleration){
+                    link->dw = parent->dw;
+                    link->dv = parent->dv + cross(parent->w, cross(parent->w, arm)) + cross(parent->dw, arm)
+                        + 2.0 * link->dq * cross(parent->w, link->sv) + link->ddq * link->sv;
+                }
+            }
             break;
 
-		case Link::FIXED_JOINT:
+        case Link::FIXED_JOINT:
         default:
             link->R = parent->R;
-			link->p = parent->R * link->b + parent->p;
+            link->p = parent->R * link->b + parent->p;
 
-			if(calcVelocity){
-				link->w = parent->w;
-				link->v = parent->v;
+            if(calcVelocity){
+                link->w = parent->w;
+                link->v = parent->v;
 
-				if(calcAcceleration){
-					link->dw = parent->dw;
-					link->dv = parent->dv;
-				}
-			}
-			break;
+                if(calcAcceleration){
+                    link->dw = parent->dw;
+                    link->dv = parent->dv;
+                }
+            }
+            break;
         }
     }
 }
