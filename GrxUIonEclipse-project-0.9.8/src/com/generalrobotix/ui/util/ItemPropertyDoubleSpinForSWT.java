645e7cc07f86c9e0afab45ae282496b93c8e700d
k.saeki <k.saeki@5146e84e-5855-4829-b829-26b81c797afe>
2010-01-08 02:57:40 +0000

    チケット#168の修正 浮動小数型対応数値入力ボックスの改善

--- GrxUIonEclipse-project-0.9.8/src/com/generalrobotix/ui/util/ItemPropertyDoubleSpinForSWT.java
+++ GrxUIonEclipse-project-0.9.8/src/com/generalrobotix/ui/util/ItemPropertyDoubleSpinForSWT.java
@@ -4,14 +4,21 @@ package com.generalrobotix.ui.util;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseTrackListener;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
 
 import com.generalrobotix.ui.GrxBaseItem;
@@ -28,9 +35,17 @@ public class ItemPropertyDoubleSpinForSWT extends Composite{
     private double step_;
     
     private Text text_;
+    private Button buttonUP_;
+    private Button buttonDOWN_;
     
     private GrxBaseItem item_;
     private String key_;
+
+    private int mouseBtnRepeat_ = 200;
+    private boolean isMouseOverBtnUp_ = false;
+    private boolean isMouseOverBtnDown_ = false;
+    private Runnable incrementRun_;
+    private Runnable decrementRun_;
     
     public ItemPropertyDoubleSpinForSWT(Composite parent,int style,double min,double max,double step){
         super(parent,style);
@@ -40,7 +55,10 @@ public class ItemPropertyDoubleSpinForSWT extends Composite{
         min_ = min;
         step_ = step;
         
-        setLayout(new GridLayout(2,false));
+        GridLayout gl =new GridLayout(2,false);
+        gl.horizontalSpacing = 0;
+        gl.verticalSpacing = 0;
+        setLayout(gl);
         
         text_ = new Text(this,SWT.SINGLE | SWT.BORDER);
         text_.addFocusListener(new FocusListener(){
@@ -50,36 +68,133 @@ public class ItemPropertyDoubleSpinForSWT extends Composite{
         	public void focusGained(FocusEvent e){
         	}
         });
+        text_.addVerifyListener(new VerifyListener(){
+            public void verifyText(VerifyEvent e){
+            	// 最大値が無限大のときは無限大文字列は受け取る
+                if(max_ == Double.POSITIVE_INFINITY)
+                {
+                    if(e.text.equals(new SEDouble(Double.POSITIVE_INFINITY).toString()))
+                        return;
+                }
+
+                for(int i = 0; i < e.text.length(); i++){
+                    char c = e.text.charAt(i);
+                    if(!(('0' <= c && c <= '9') ||
+                         c == '.' ||
+                         c == '-' ||
+                         c == 'e' || c == 'E'))
+                    {
+                        e.doit = false;
+                    }
+                }
+            }
+        });
+        
         GridData gridData = new GridData();
+        gridData.verticalSpan = 2;
         gridData.widthHint = 80;
         text_.setLayoutData(gridData);
         
-        Composite buttonPane = new Composite(this,SWT.NONE);
-        buttonPane.setLayout(new RowLayout(SWT.VERTICAL));
-        Button buttonUP = new Button(buttonPane,SWT.ARROW | SWT.UP);
-        buttonUP.addSelectionListener(new SelectionListener(){
-
-            public void widgetDefaultSelected(SelectionEvent e) {
+        buttonUP_ = new Button(this,SWT.ARROW | SWT.UP);
+        buttonUP_.addMouseTrackListener(new MouseTrackListener(){
+            public void mouseEnter(MouseEvent e){
+                isMouseOverBtnUp_ = true;
             }
-
-            public void widgetSelected(SelectionEvent e) {
-                double v = text2value() + step_;
-                updateProperty(v);
+            public void mouseExit(MouseEvent e){
+                isMouseOverBtnUp_ = false;
             }
-            
+            public void mouseHover(MouseEvent e){
+            }
         });
-        Button buttonDOWN = new Button(buttonPane,SWT.ARROW | SWT.DOWN);
-        buttonDOWN.addSelectionListener(new SelectionListener(){
 
-            public void widgetDefaultSelected(SelectionEvent e) {
+        buttonUP_.addMouseMoveListener(new MouseMoveListener(){
+            public void mouseMove(MouseEvent e){
+                isMouseOverBtnUp_ = (0 <= e.x && e.x < buttonUP_.getSize().x && 0 <= e.y && e.y < buttonUP_.getSize().y);
             }
-
-            public void widgetSelected(SelectionEvent e) {
-                double v = text2value() - step_;
-                updateProperty(v);
-            }
-            
         });
+        buttonUP_.addMouseListener(new MouseListener(){
+            public void mouseDoubleClick(MouseEvent e){
+            }
+            public void mouseDown(MouseEvent e) {
+                if(e.button == 1){
+                    incrementValue();
+                    startIncrementTimer();
+                }
+            }
+            public void mouseUp(MouseEvent e) {
+                if(e.button == 1){
+                    stopIncrementTimer();
+                }
+            }
+        });
+        
+        buttonDOWN_ = new Button(this,SWT.ARROW | SWT.DOWN);
+        buttonDOWN_.addMouseTrackListener(new MouseTrackListener(){
+            public void mouseEnter(MouseEvent e){
+                isMouseOverBtnDown_ = true;
+            }
+            public void mouseExit(MouseEvent e){
+                isMouseOverBtnDown_ = false;
+            }
+            public void mouseHover(MouseEvent e){
+            }
+        });
+
+        buttonDOWN_.addMouseMoveListener(new MouseMoveListener(){
+            public void mouseMove(MouseEvent e){
+                isMouseOverBtnDown_ = (0 <= e.x && e.x < buttonDOWN_.getSize().x && 0 <= e.y && e.y < buttonDOWN_.getSize().y);
+            }
+        });
+        buttonDOWN_.addMouseListener(new MouseListener(){
+            public void mouseDoubleClick(MouseEvent e){
+            }
+            public void mouseDown(MouseEvent e) {
+                if(e.button == 1){
+                    decrementValue();
+                    startDecrementTimer();
+                }
+            }
+            public void mouseUp(MouseEvent e) {
+                if(e.button == 1){
+                    stopDecrementTimer();
+                }
+            }
+        });
+        
+        text_.addControlListener(new ControlListener(){
+            public void controlMoved(ControlEvent e) {
+            } 
+            public void controlResized(ControlEvent e) {
+                GridData buttonGrid = new GridData();
+                buttonGrid.heightHint= text_.getSize().y/2;
+                buttonUP_.setLayoutData(buttonGrid);
+                buttonDOWN_.setLayoutData(buttonGrid);
+                layout();
+            }
+        });
+        
+        incrementRun_ = new Runnable() {
+            public void run() { 
+                Display display = text_.getDisplay();
+                if (!display.isDisposed())
+                {
+                    if(isMouseOverBtnUp_)
+                        incrementValue();
+                    display.timerExec(mouseBtnRepeat_, this);
+                }
+            }
+        };
+        decrementRun_ = new Runnable() {
+            public void run() { 
+                Display display = text_.getDisplay();
+                if (!display.isDisposed())
+                {
+                    if(isMouseOverBtnDown_)
+                        decrementValue();
+                    display.timerExec(mouseBtnRepeat_, this);
+                }
+            }
+        };
         
         setValue((max+min)/2);
     }
@@ -131,5 +246,42 @@ public class ItemPropertyDoubleSpinForSWT extends Composite{
     
     public double getValue(){
     	return value_.doubleValue();
+    }
+
+    private void incrementValue(){
+        double v = text2value() + step_;
+        updateProperty(v);
+    }
+    private void decrementValue(){
+        double v = text2value() - step_;
+        updateProperty(v);
+    }
+    private void startIncrementTimer(){
+        Display display = text_.getDisplay();
+        if (!display.isDisposed())
+        {
+            display.timerExec(mouseBtnRepeat_, incrementRun_);
+        }
+    }
+    private void stopIncrementTimer(){
+        Display display = text_.getDisplay();
+        if (!display.isDisposed())
+        {
+            display.timerExec(-1, incrementRun_);
+        }
+    }
+    private void startDecrementTimer(){
+        Display display = text_.getDisplay();
+        if (!display.isDisposed())
+        {
+            display.timerExec(mouseBtnRepeat_, decrementRun_);
+        }
+    }
+    private void stopDecrementTimer(){
+        Display display = text_.getDisplay();
+        if (!display.isDisposed())
+        {
+            display.timerExec(-1, decrementRun_);
+        }
     }
 }
