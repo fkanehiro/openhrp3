94d663629eee16caf69ab075cdb93746524dc08c
k.saeki <k.saeki@5146e84e-5855-4829-b829-26b81c797afe>
2010-02-17 08:12:43 +0000

    チケット#1232の修正 OpenHRPViewPartでも他のViewPartと同様にGrxBaseViewPart::createViewによりOpenHRPViewをnewするように修正。GrxJythonPromptViewPart, GrxRobotStatViewPartでも同様の起動方法に修正。

--- GrxUIonEclipse-project-0.9.8/src/com/generalrobotix/ui/view/GrxOpenHRPViewPart.java
+++ GrxUIonEclipse-project-0.9.8/src/com/generalrobotix/ui/view/GrxOpenHRPViewPart.java
@@ -8,14 +8,7 @@ import com.generalrobotix.ui.grxui.Activator;
 public class GrxOpenHRPViewPart extends GrxBaseViewPart {
 
     public void createPartControl(Composite parent) {
-		Activator act = Activator.getDefault();
-		if(act != null){
-			if(Activator.getDefault().manager_ == null)
-			{
-				act.tryStartGrxUI();
-			}
-            v = new GrxOpenHRPView("OpenHRP",Activator.getDefault().manager_,this,parent);
-		}
+        createView( GrxOpenHRPView.class, "OpenHRP", this, parent );
     }
 
 }
