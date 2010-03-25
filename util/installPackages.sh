bd77bb7cc7d25ba9ddca0c9e4e4af2eb6e3008a8
warataka <warataka@5146e84e-5855-4829-b829-26b81c797afe>
2010-03-25 06:23:49 +0000

    http://www.openrtm.org/pub/Linux/ubuntu/ にtvmetが登録されているのでtvmetも自動的導入できるようなスクリプトに変更

--- util/installPackages.sh
+++ util/installPackages.sh
@@ -6,22 +6,7 @@ if [ "$arg" = "" ]; then
   echo "EXAMPLE : $0 packages.list.ubt"
   echo
 else
-  sudo aptitude update
-  sudo aptitude install `sed -e '/^#/D' $1 | xargs`
   sudo ${0%/*}/pkg_install_ubuntu.sh
+  sudo apt-get --force-yes install `sed -e '/^#/D' $1 | xargs`
   sudo update-java-alternatives -s java-6-sun
-
-  if [ "`dpkg -l | grep tvmet`" = "" ] ; then
-    echo
-    echo "#############################################################################"
-    echo
-    echo " After complete packge installation."
-    echo " Please download and Install tvmet manually."
-    echo ""
-    echo " tvmet  : http://www.openrtp.jp/openhrp3/download/tvmet_1.7.1-1_i386.deb"
-    echo 
-    echo "#############################################################################"
-    echo
-  fi
-
 fi
