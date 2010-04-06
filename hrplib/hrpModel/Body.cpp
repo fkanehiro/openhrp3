df37361112f6bbe759277971be54886376e43d01
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2010-04-06 07:10:15 +0000

    added Body::calcCMJacobian()

--- hrplib/hrpModel/Body.cpp
+++ hrplib/hrpModel/Body.cpp
@@ -814,3 +814,59 @@ bool CustomizedJointPath::hasAnalyticalIK()
 {
     return (ikTypeId != 0);
 }
+
+void Body::calcCMJacobian(Link *base, dmatrix &J)
+{
+    // prepare subm, submwc
+    JointPathPtr jp = getJointPath(rootLink(), base);
+    Link *skip = jp->joint(0);
+    skip->subm = rootLink()->m;
+    skip->submwc = rootLink()->m*rootLink()->wc;
+    Link *l = rootLink()->child;
+    if (l){
+        if (l != skip) {
+            l->calcSubMassCM();
+            skip->subm += l->subm;
+            skip->submwc += l->submwc;
+        }
+        l = l->sibling;
+        while(l){
+            if (l != skip){
+                l->calcSubMassCM();
+                skip->subm += l->subm;
+                skip->submwc += l->submwc;
+            }
+            l = l->sibling;
+        }
+    }
+    
+    // assuming there is no branch between base and root
+    for (int i=1; i<jp->numJoints(); i++){
+        l = jp->joint(i);
+        l->subm = l->parent->m + l->parent->subm;
+        l->submwc = l->parent->m*l->parent->wc + l->parent->submwc;
+    }
+    
+    // compute Jacobian
+    J.resize(3, numJoints());
+    
+    std::vector<int> sgn(numJoints(), 1);
+    for (int i=0; i<jp->numJoints(); i++) sgn[jp->joint(i)->jointId] = -1;
+    
+    for (int i=0; i<numJoints(); i++){
+        Link *j = joint(i);
+        switch(j->jointType){
+        case Link::ROTATIONAL_JOINT:
+        {
+            Vector3 omega(sgn[j->jointId]*j->R*j->a);
+            Vector3 arm((j->submwc-j->subm*j->p)/totalMass_);
+            Vector3 dp(cross(omega, arm));
+            setVector3(dp, J, 0, j->jointId);
+            break;
+        }
+        default:
+            std::cerr << "calcCMJacobian() : unsupported jointType("
+                      << j->jointType << std::endl;
+        }
+    }
+}
