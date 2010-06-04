2581eb633509a42fecf85aedf2524dd37263b9ae
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2010-06-04 07:58:12 +0000

    added examples

--- sample/script/test.py
+++ sample/script/test.py
@@ -1,6 +1,13 @@
 a = 1
 b = 10
 waitInputSetMessage("$1= test ")
-waitInputMenu([["A","print a","B","print b"],["A*10","print a*10","B*10","print b*10"],["A*100","print a*100","B*100","print b*100"]])
+waitInputMenu([["A","print a","B","print b",
+                '----------label----------', '#label',
+                'string input', 'print #T',
+                'double input', 'print #D,#D',
+                'int input', 'print #I,#I,#I',
+                ],
+               ["A*10","print a*10","B*10","print b*10"],
+               ["A*100","print a*100","B*100","print b*100"]])
 
 
