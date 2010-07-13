8bfe431dcab1c28c8a01eaf6d017b26105761e68
nakaoka <nakaoka@5146e84e-5855-4829-b829-26b81c797afe>
2010-07-13 07:15:45 +0000

    improved Coldet API

--- hrplib/hrpModel/ColdetLinkPair.h
+++ hrplib/hrpModel/ColdetLinkPair.h
@@ -6,20 +6,34 @@
 #define HRPMODEL_COLDET_LINK_PAIR_H_INCLUDED
 
 #include <hrpCollision/ColdetModelPair.h>
+#include "Link.h"
 #include "Config.h"
 
 namespace hrp {
-
+    
     class Link;
-
+    
     class HRPMODEL_API ColdetLinkPair : public ColdetModelPair
     {
       public:
-        ColdetLinkPair(Link* link1, Link* link2);
-        ColdetLinkPair(const ColdetLinkPair& org);
-        virtual ~ColdetLinkPair();
+        ColdetLinkPair(Link* link1, Link* link2)
+            : ColdetModelPair(link1->coldetModel, link2->coldetModel) {
+            links[0] = link1;
+            links[1] = link2;
+        }
         
-        void updatePositions();
+        ColdetLinkPair(const ColdetLinkPair& org)
+            : ColdetModelPair(org) {
+            links[0] = org.links[0];
+            links[1] = org.links[1];
+        }
+        
+        virtual ~ColdetLinkPair() { }
+        
+        void updatePositions() {
+            model(0)->setPosition(links[0]->R, links[0]->p);
+            model(1)->setPosition(links[1]->R, links[1]->p);
+        }
         
         hrp::Link* link(int index) { return links[index]; }
         
