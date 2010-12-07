d52ec903eb4c573506adc2a812b7a1426c970d69
k.saeki <k.saeki@5146e84e-5855-4829-b829-26b81c797afe>
2010-12-07 00:54:03 +0000

    レポジトリのリストにあるものだけをapt-get installするように修正しました。 また、openrtmのリポジトリがなくても終了しないように修正しました。

--- util/installPackages.sh
+++ util/installPackages.sh
@@ -5,8 +5,36 @@ if [ "$arg" = "" ]; then
   echo "USAGE   : $0 PACKAGES.LIST" 
   echo "EXAMPLE : $0 packages.list.ubt"
   echo
-else
-  sudo ${0%/*}/pkg_install_ubuntu.sh
-  sudo apt-get --force-yes install `sed -e '/^#/D' $1 | xargs`
-  sudo update-java-alternatives -s java-6-sun
+  exit 1
+fi
+
+sudo ${0%/*}/pkg_install_ubuntu.sh
+
+pkgnames=`apt-cache pkgnames`
+for package in `sed -e '/^#/D' $arg | xargs`
+do
+  if echo "$pkgnames" | grep "^$package$" >/dev/null; then
+    if [ -n ok_pkgs ]; then
+      ok_pkgs="$ok_pkgs $package"
+    else
+      ok_pkgs=$package
+    fi
+  else
+    if [ -n ng_pkgs ]; then
+      ng_pkgs="$ng_pkgs $package"
+    else
+      ng_pkgs=$package
+    fi
+  fi
+done
+
+sudo apt-get --force-yes install $ok_pkgs
+sudo update-java-alternatives -s java-6-sun
+
+if [ -n ng_pkgs ]; then
+  echo "Packge installation is incomplete."
+  echo "Please download and install these packages:"
+  echo $ng_pkgs
+  echo
+  exit 1
 fi
