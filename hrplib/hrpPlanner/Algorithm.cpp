5b86dca8622e41a19699dfa438dc58b2a4dff2c8
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2010-12-16 03:21:18 +0000

    modified preparePlanning() to check if the goal is reachable without collision-check

--- hrplib/hrpPlanner/Algorithm.cpp
+++ hrplib/hrpPlanner/Algorithm.cpp
@@ -96,6 +96,12 @@ bool Algorithm::preparePlanning()
       std::cerr << "goal configuration is not collision-free" << std::endl;
       return false;
   }
+  Mobility *mobility = planner_->getMobility();
+  if (!mobility->isReachable(start_, goal_, false)){
+      std::cerr << "goal is not reachable even if collision is not checked"
+                << std::endl;
+      return false;
+  }
 
   return true;
 }
