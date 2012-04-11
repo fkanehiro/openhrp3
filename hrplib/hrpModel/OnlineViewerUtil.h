9dae0c78c4b65da9ae5d54c4762ddd07ba5fe366
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2012-04-11 00:36:04 +0000

    added initWorldState(OpenHRP::WorldState&, hrp::Worldbase&, std::vector<hrp::ColdetLinkPairPtr>&)

--- hrplib/hrpModel/OnlineViewerUtil.h
+++ hrplib/hrpModel/OnlineViewerUtil.h
@@ -4,6 +4,7 @@
 #include <hrpCorba/OpenHRPCommon.hh>
 #include <hrpModel/Body.h>
 #include <hrpModel/World.h>
+#include <hrpModel/ColdetLinkPair.h>
 
 void setupCharacterPosition(OpenHRP::CharacterPosition &characterPosition, 
                              hrp::BodyPtr body); 
@@ -11,5 +12,7 @@ void updateCharacterPosition(OpenHRP::CharacterPosition &characterPosition,
                              hrp::BodyPtr body);
 void getWorldState(OpenHRP::WorldState& state,  hrp::WorldBase& world);
 void initWorldState(OpenHRP::WorldState& state, hrp::WorldBase& world);
+void initWorldState(OpenHRP::WorldState& state, hrp::WorldBase& world,
+                    std::vector<hrp::ColdetLinkPairPtr>& pairs);
 
 #endif
