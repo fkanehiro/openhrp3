e9bf8d104ddcd59686d508111b0cd35dcea47cef
kanehiro <kanehiro@5146e84e-5855-4829-b829-26b81c797afe>
2012-06-25 13:19:18 +0000

    adds GrxRTSItem for hrpsys-simulator

---
+++ GrxUIonEclipse-project-0.9.8/src/com/generalrobotix/ui/item/GrxRTSItem.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2008, AIST, the University of Tokyo and General Robotix Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * General Robotix Inc.
+ * National Institute of Advanced Industrial Science and Technology (AIST)
+ */
+
+/*
+ *  GrxRTSlItem.java
+ *
+ *  @author Yuichiro Kawasumi (General Robotix, Inc.)
+ *  @author Shin'ichiro Nakaoka (AIST)
+ */
+
+package com.generalrobotix.ui.item;
+
+import com.generalrobotix.ui.GrxBaseItem;
+import com.generalrobotix.ui.GrxPluginManager;
+
+@SuppressWarnings("serial")
+public class GrxRTSItem extends GrxBaseItem{
+	
+    /**
+     * @constructor
+     * @param name
+     * @param manager
+     */
+    public GrxRTSItem(String name, GrxPluginManager manager) {
+	super(name, manager);
+	System.out.println("GrxRTSItem");
+    }
+}
