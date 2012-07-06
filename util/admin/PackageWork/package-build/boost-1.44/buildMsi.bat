abd689cb0e5fce6a989c9f1f4e4942ef8d6a2f7e
hattori <hattori@5146e84e-5855-4829-b829-26b81c797afe>
2012-07-06 01:31:26 +0000

    VC10用の修正

--- util/admin/PackageWork/package-build/boost-1.44/buildMsi.bat
+++ util/admin/PackageWork/package-build/boost-1.44/buildMsi.bat
@@ -1,4 +1,4 @@
 ruby collect.rb
 ruby create_wxs.rb
 candle boost.wxs -out boost.wixobj
-light -ext WixUIExtension -out boost-1.44_vc9.msi boost.wixobj
+light -ext WixUIExtension -out boost-1.44.msi boost.wixobj
