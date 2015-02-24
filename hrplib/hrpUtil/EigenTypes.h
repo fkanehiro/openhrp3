f22b77e136275699a7e7de34439221350bf2d462
Kei Okada <kei.okada@gmail.com>
2015-02-23 20:44:42 -0800

    [EigenTypes.h] add output operator

--- hrplib/hrpUtil/EigenTypes.h
+++ hrplib/hrpUtil/EigenTypes.h
@@ -17,4 +17,27 @@ namespace hrp{
     typedef Eigen::Quaternion<double> dquaternion;
 };
 
+#include <iostream>
+inline std::ostream& operator<<(std::ostream& out, hrp::dmatrix &a) {
+    const int c = a.rows();
+    const int n = a.cols();
+
+    for(int i = 0; i < c; i++){
+        out << "      :";
+        for(int j = 0; j < n; j++){
+            out << " " << std::setw(7) << std::setiosflags(std::ios::fixed) << std::setprecision(4) << (a)(i,j);
+        }
+        out << std::endl;
+    }
+}
+
+inline std::ostream& operator<<(std::ostream& out, hrp::dvector &a) {
+    const int n = a.size();
+
+    for(int i = 0; i < n; i++){
+        out << std::setw(7) << std::setiosflags(std::ios::fixed) << std::setprecision(4) << a(i) << " ";
+    }
+    out << std::endl;
+}
+
 #endif
