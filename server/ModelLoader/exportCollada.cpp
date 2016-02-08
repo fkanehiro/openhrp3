c4ff23631745d2085bceccec468f94ea6e122845
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2016-02-08 18:55:14 +0900

    [exportCollada] fix help message for adding information of manipulator to collada file

--- server/ModelLoader/exportCollada.cpp
+++ server/ModelLoader/exportCollada.cpp
@@ -135,7 +135,7 @@ int main(int argc, char* argv[])
 	{
 	  std::cerr << "Usage: " << argv[0] 
 		    << "-i <inFileName> " << "-o <outFileName> "
-		    << "-a <armname>,<armbaselink>,<armeffectorlink>,<px>,<py>,<pz>,<qx>,<qy>,<qz>,<qw>,<gripperjoint>,<gripperdir>..."
+		    << "-a <manipulator_name>,<frame_origin>,<frame_tip>,<px>,<py>,<pz>,<rotX>,<rotY>,<rotZ>,<rotTheta[rad]>,<gripper_joint>,<closing_direction>...\n see http://openrave.org/docs/latest_stable/collada_robot_extensions/"
 		    << std::endl;
 	}
 #endif    
